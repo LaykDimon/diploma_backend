f57422a01dca7477a689a7fbdf781e99d3007f34
Dmitry <61393026+LaykDimon@users.noreply.github.com>
2022-04-05 03:10:15 +0300

    fixed lemmatizator

--- analyzer_api/serializers.py
+++ analyzer_api/serializers.py
@@ -2,7 +2,7 @@ from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
 
 class BaseTextSerializer (serializers.Serializer):
-    input_text = serializers.CharField(max_length = 20000)
+    input_text = serializers.CharField(max_length = 30000)
 
 class URLSerializer (serializers.Serializer):
     url = serializers.URLField(max_length = 255)
--- analyzer_api/utils.py
+++ analyzer_api/utils.py
@@ -1,23 +1,28 @@
 import math
 import spacy
+import re
 from collections import Counter
 
 #rus
 nlp = spacy.load("ru_core_news_sm")
 #eng
-nlp2 = spacy.load("en_core_web_sm")
+# nlp = spacy.load("en_core_web_sm")
 
-lemmatizer = nlp2.get_pipe("lemmatizer")
+lemmatizer = nlp.get_pipe("lemmatizer")
 
 def get_analyze_results(text):
     
     nlp_doc = nlp(text)
+    # print(Counter(nlp_doc).most_common(50))
     # lemmatizer = nlp.get_pipe("lemmatizer")
     
-    cleaned_nlp_doc = [token.text for token in nlp_doc if not token.is_stop and not token.is_punct and token.pos_ is 'NOUN']
-    
-    # lemmatized_nlp_doc = [token.lemma_ for token in cleaned_nlp_doc]
-    # print(lemmatized_nlp_doc)
+    # cleaned_nlp_doc = [token.text for token in nlp_doc if not token.is_stop and not token.is_punct]
+    # print(Counter(cleaned_nlp_doc).most_common(50))
+    # print(cleaned_nlp_doc)
+
+    lemmatized_nlp_doc = [token.lemma_ for token in nlp_doc if not token.is_stop and not token.is_punct]
+    lemmatized_nlp_doc = [element for element in lemmatized_nlp_doc if element.strip()]
+    # print(Counter(lemmatized_nlp_doc).most_common(50))
 
     analyze_results = {}
 
@@ -31,7 +36,7 @@ def get_analyze_results(text):
 
     analyze_results["water_content"] = get_water_content(nlp_doc)
 
-    analyze_results["classic_nausea"] = get_classic_nausea(cleaned_nlp_doc)
+    analyze_results["classic_nausea"] = get_classic_nausea(lemmatized_nlp_doc)
 
     analyze_results["academic_nausea"] = get_academic_nausea(nlp_doc)
 
@@ -57,17 +62,21 @@ def get_sentence_count(nlp_document):
 def get_water_content(nlp_document):
     water_word_amount = len([token for token in nlp_document if token.is_stop])
     water_content = round(water_word_amount / get_word_count(nlp_document) * 100, 2)
+    print(water_word_amount)
     return water_content
 
 def get_classic_nausea(nlp_document):
     word_frequency = Counter(nlp_document)
     most_common_word = word_frequency.most_common(1)
     classic_nausea = round(math.sqrt(most_common_word[0][1]), 2)
+    print(most_common_word)
     return classic_nausea
 
 def get_academic_nausea(nlp_document):
-    cleaned_nlp_doc = [token.text for token in nlp_document if not token.is_stop and not token.is_punct and token.pos_ is 'NOUN']
-    word_frequency = Counter(cleaned_nlp_doc)
+    lemmatized_nlp_doc = [token.lemma_ for token in nlp_document if not token.is_stop and not token.is_punct]
+    lemmatized_nlp_doc = [element for element in lemmatized_nlp_doc if element.strip()]
+    word_frequency = Counter(lemmatized_nlp_doc)
     most_common_word = word_frequency.most_common(1)
+    print(word_frequency.most_common(50 ))
     academic_nausea = round(most_common_word[0][1] / get_word_count(nlp_document) * 100, 2)
     return academic_nausea
